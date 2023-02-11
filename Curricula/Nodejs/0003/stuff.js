2a8b6f5977fc16cada6c547adebd35be50202025
Mingjun Geng <sooofeve@gmail.com>
2023-02-10 16:01:26 -0800

    Node  Nodejs/0003 --- step 3.3

--- Curricula/Nodejs/0003/stuff.js
+++ Curricula/Nodejs/0003/stuff.js
@@ -1,9 +1,14 @@
-module.exports.counter = function(arr){
+var  counter = function(arr){
     return 'There are ' + arr.length + ' elements in this  array;'
 };
 
-module.exports.addr = function(a, b){
+var addr = function(a, b){
     return `The sum of the 2 numbers is ${a+b}`;
 }
-module.exports.pi = 3.14;
+var pi = 3.14;
 
+module.exports = {
+    counter : counter,
+    addr : addr,
+    pi : pi
+};
