ac26d9d34208574f958f8fed268a6e7ff9c82cbf
Mingjun Geng <sooofeve@gmail.com>
2023-02-12 01:20:04 -0800

    Node  Nodejs/0004-Node-Event/app.js --- step 2

--- Curricula/Nodejs/0004-Node-Event/app.js
+++ Curricula/Nodejs/0004-Node-Event/app.js
@@ -1,12 +1,23 @@
-var events = require ('events')
+var events = require ('events');
+var util = require('util');
 
-var myEmitter = new events.EventEmitter();
+var Person = function(name){
+    this.name = name;
+}
 
-myEmitter.on('someEvent', function(mssg){
-    console.log(mssg)
+util.inherits(Person, events.EventEmitter);
+
+var james = new Person('james');
+var mary = new Person('mary');
+var ryu = new Person('ryu');
+
+var people = [james, mary, ryu];
+
+people.forEach(function(person){
+    person.on('speak', function(mssg){
+        console.log(person.name + ' said: ' + mssg);
+    });
 });
 
-myEmitter.emit('someEvent', 'the event was emitted');
-
-
-
+james.emit('speak', 'hey dudes')
+ryu.emit('speak', 'I want a curry')
