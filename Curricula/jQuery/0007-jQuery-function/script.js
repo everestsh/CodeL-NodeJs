d6d2d85fce94ad204938d4b5494b93eae9906d7a
Mingjun Geng <sooofeve@gmail.com>
2023-02-18 14:18:20 -0800

    jQuery

---
+++ Curricula/jQuery/0007-jQuery-function/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    // $("#btnClick").click(function(){
+    //     $("div").hide();
+    //     console.log("wo shi jQuery!")
+    // })
+    jQuery("#btnClick").click(function(){
+        console.log("wo shi jQuery!")
+    })
+})
