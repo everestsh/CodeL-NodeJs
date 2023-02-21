e73564922528a2131ee6c93184e7e6f7d5b80b67
Mingjun Geng <sooofeve@gmail.com>
2023-02-21 14:03:24 -0800

    node Nodejs/NodeJSTouroalBeginners/0016-Node-Pipes

--- Curricula/Nodejs/NodeJSTouroalBeginners/0016-Node-Pipes/app.js
+++ Curricula/Nodejs/NodeJSTouroalBeginners/0016-Node-Pipes/app.js
@@ -1,11 +1,20 @@
 var http = require('http');
 var  fs = require('fs');
 
-var myReadStream = fs.createReadStream(__dirname + '/readMe.txt');
-var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt');
+var server = http.createServer(function(req, res){
+    console.log('request was made: ' + req.url)
+    res.writeHead(200,{'Content-Type': 'text/plain'});
+    var myReadStream = fs.createReadStream(__dirname + '/readMe.txt');
+    // var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt');
+    // myReadStream.pipe(myWriteStream);
+    // res.end('Hey ninjas');
+    myReadStream.pipe(res)
+});
+server.listen(3000, '127.0.0.1');
+console.log('yo dawgs, now listening to port 3000')
 
 
-myReadStream.pipe(myWriteStream);
+
 
 // myReadStream.on('data', function(chunk){
 //     console.log('new chunk received:');
