feeb2a964c475bee34a483b0106d59eaf1669597
Mingjun Geng <sooofeve@gmail.com>
2023-02-24 19:43:15 -0800

    node Nodejs/NodeJSTouroalBeginners/0027-Node-partialTemplate

---
+++ Curricula/Nodejs/NodeJSTouroalBeginners/0027-Node-partialTemplate/app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+
+var app = express();
+app.set('view engine', 'ejs')
+
+app.get('/', function(req, res){
+    // res.send('this is the homepage')
+    res.sendFile(__dirname + '/index.html')
+})
+app.get('/contact', function(req, res){
+    // res.send('this is the contact page')
+    res.sendFile(__dirname + '/contact.html')
+})
+app.get('/profile/:name', function(req, res){
+    // res.send('You requested to see a profile with the id of ' + req.params.name)
+    var data = {age:29, job:'ninja', hobbies: ['eating', 'fighting', 'fishing']}
+    res.render('profile', {person: req.params.name, data: data});
+});
+app.listen(3000);
