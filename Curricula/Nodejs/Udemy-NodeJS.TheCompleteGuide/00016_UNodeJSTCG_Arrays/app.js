9f7fbd370a5097ef15ef04af89104eddcdb1671b
Mingjun Geng <sooofeve@gmail.com>
2023-02-27 12:44:58 -0800

    node Nodejs/Udemy-NodeJS.TheCompleteGuide/00016_UNodeJSTCG_Arrays

---
+++ Curricula/Nodejs/Udemy-NodeJS.TheCompleteGuide/00016_UNodeJSTCG_Arrays/app.js
@@ -0,0 +1,16 @@
+const person = {
+    name: 'Max',
+    age: 29, 
+
+    // run: person.greet()  ==>  Hi, I am Max
+    greet(){
+        console.log("Hi, I am "+this.name)
+    }
+}
+
+const hobbies = ['Sports', 'Cooking']
+// for(let hobby of hobbies){
+//     console.log(hobby)
+// }
+console.log(hobbies.map(hobby=> 'Hobby: '+hobby));
+console.log(hobbies);
