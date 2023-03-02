692978b03207d70303e11ed846efbb315292996b
Mingjun Geng <sooofeve@gmail.com>
2023-03-01 20:08:34 -0800

    node Nodejs/Udemy-NodeJS.TheCompleteGuide/√

--- Curricula/Nodejs/Udemy-NodeJS.TheCompleteGuide/00034_UNodeJSTCG_requestBodies/app.js
+++ Curricula/Nodejs/Udemy-NodeJS.TheCompleteGuide/00034_UNodeJSTCG_requestBodies/app.js
@@ -13,11 +13,17 @@ const server = http.createServer((req, res)=>{
     }
     if(url==='/message' && method==='POST'){
         const body = [];
-        req.on('data', (chuck)=>{
+        req.on('data', (chunk)=>{
             console.log(chunk)
             body.push(chunk)
         })
-        fs.writeFileSync('message.txt', 'DUMMY');
+        req.on('end', ()=>{
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        })
+        // fs.writeFileSync('message.txt', 'DUMMY');
         res.statusCode = 302;
         res.setHeader('Location', '/');
         return res.end();
