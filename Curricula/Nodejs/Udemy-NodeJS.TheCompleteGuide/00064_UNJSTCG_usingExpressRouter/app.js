871e93b84006dc768f1256d8e64480bfcd464d66
Mingjun Geng <sooofeve@gmail.com>
2023-03-23 19:18:55 -0700

    node Nodejs/Udemy-NodeJS.TheCompleteGuide/00064_UNJSTCG_usingExpressRouter

--- Curricula/Nodejs/Udemy-NodeJS.TheCompleteGuide/00064_UNJSTCG_usingExpressRouter/app.js
+++ Curricula/Nodejs/Udemy-NodeJS.TheCompleteGuide/00064_UNJSTCG_usingExpressRouter/app.js
@@ -3,38 +3,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop')
+
 app.use(bodyParser.urlencoded({extended:false}));
-// app.use((req, res, next)=>{
-//     console.log('In the middleware!');
-//     next();
-// });
-// app.use((req, res, next)=>{
-//     console.log('In anther middleware!');
-//     res.send('<h1>Hello from Express!</h1>')
-// });
-// const server = http.createServer(app)
-// server.listen(3000);
 
 app.use('/', (req, res, next)=>{
     console.log('this alway run!');
     next();
 })
 
-app.use('/add-product', (req, res, next)=>{
-    console.log('In another middleware!');
-    // res.send('<h1>The "Add Product" Page!</h1>');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"> <button type="submit">Add Product</button></form>');
-});
+app.use(adminRoutes)
 
-app.use('/product', (req, res, next)=>{
-    console.log(req.body);
-    res.redirect("/");
-})
-
-app.use('/', (req, res, next)=>{
-    console.log('In another middleware!');
-    res.send('<h1>Hello from Express!</h1>');
-})
+app.use(shopRoutes)
 
 app.listen(3000);
 
