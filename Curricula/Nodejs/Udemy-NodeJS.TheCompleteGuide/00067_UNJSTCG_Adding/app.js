ad18d1bc0f903afa01c19d0d1844c6b54abf8509
Mingjun Geng <sooofeve@gmail.com>
2023-03-23 19:20:14 -0700

    node Nodejs/Udemy-NodeJS.TheCompleteGuide/00066_UNJSTCG_usingExpressRouter

---
+++ Curricula/Nodejs/Udemy-NodeJS.TheCompleteGuide/00067_UNJSTCG_Adding/app.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop')
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/', (req, res, next)=>{
+    console.log('this alway run!');
+    next();
+})
+
+app.use(adminRoutes)
+
+app.use(shopRoutes)
+
+app.listen(3000);
+
