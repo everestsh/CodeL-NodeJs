809ce65b014303517c89a923429870b83e9a50a4
Mingjun Geng <sooofeve@gmail.com>
2023-03-24 19:19:39 -0700

    node Nodejs/Udemy-NodeJS.TheCompleteGuide/00067_UNJSTCG_Addinga404ErrorPage

---
+++ Curricula/Nodejs/Udemy-NodeJS.TheCompleteGuide/00067_UNJSTCG_Addinga404ErrorPage/app.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop')
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+// app.use('/', (req, res, next)=>{
+//     console.log('this alway run!');
+//     next();
+// })
+
+app.use(adminRoutes)
+
+app.use(shopRoutes)
+
+app.use((req, res, next)=>{
+    res.status(404).send('<h1>Page not found</h1>')
+})
+app.listen(3000);
+
