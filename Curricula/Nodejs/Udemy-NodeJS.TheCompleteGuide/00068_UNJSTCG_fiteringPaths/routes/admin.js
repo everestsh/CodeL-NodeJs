c4b3d3290909478408595c5838e8c1762146abf0
Mingjun Geng <sooofeve@gmail.com>
2023-03-24 19:38:56 -0700

    node Nodejs/Udemy-NodeJS.TheCompleteGuide/00068_UNJSTCG_fiteringPaths

--- Curricula/Nodejs/Udemy-NodeJS.TheCompleteGuide/00068_UNJSTCG_fiteringPaths/routes/admin.js
+++ Curricula/Nodejs/Udemy-NodeJS.TheCompleteGuide/00068_UNJSTCG_fiteringPaths/routes/admin.js
@@ -2,13 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 
-router.use('/add-product', (req, res, next)=>{
+router.get('/add-product', (req, res, next)=>{
     console.log('In another middleware!');
     // res.send('<h1>The "Add Product" Page!</h1>');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"> <button type="submit">Add Product</button></form>');
+    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"> <button type="submit">Add Product</button></form>');
 });
 
-router.use('/product', (req, res, next)=>{
+router.post('/add-product', (req, res, next)=>{
     console.log(req.body);
     res.redirect("/");
 })
