77cb13cbab0bb91445dbd37e268f06d554485e26
Mingjun Geng <sooofeve@gmail.com>
2023-04-04 21:54:21 -0700

    node Nodejs/Udemy-NodeJS.TheCompleteGuide/00070_UNJSTCG_servingHTMLPages c

---
+++ Curricula/Nodejs/Udemy-NodeJS.TheCompleteGuide/00071_UNJSTCG_returningA404Page/routes/admin.js
@@ -0,0 +1,18 @@
+const path = require('path')
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/add-product', (req, res, next)=>{
+    console.log('In another middleware!');
+    // res.send('<h1>The "Add Product" Page!</h1>');
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
+    // res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"> <button type="submit">Add Product</button></form>');
+});
+
+router.post('/add-product', (req, res, next)=>{
+    console.log(req.body);
+    res.redirect("/");
+})
+
+module.exports = router;
